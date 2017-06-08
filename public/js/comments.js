8a2d71f70d53ec20c691eb585640ea65a1fb4801
qjj <525617699@qq.com>
2017-06-08 10:44:57 +0800

    用laytpl.js 实现评论的模板实现   互评

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,40 +1,53 @@
 
-
-/*String.prototype.temp = function(obj) {
-    return this.replace(/\$\w+\$/gi, function(matchs) {
-        var str = obj[matchs.replace(/\$/g, "")];
-        return (str + "") == "undefined"? "": str;
-    });
-};*/
-
 // 回复别人的评论
-$('.replyyyyy').on('click', function (e) {
+$('.comment-list').on('click', '.reply', function () {
 
-    console.log('click!!!!');
     var target = $(this);
     var toID = target.data('tid');
     var commentID = target.data('cid');
-    console.log(toID);
-    console.log(commentID);
+    // console.log(toID);
+    // console.log(commentID);
 
     //插入隐藏域  .textarea-container
+    if($('#toID').length > 0){
+        $('#toID').attr('value', toID);
+    }
+    else{
+        $('<input>').attr({
+            type: 'hidden',
+            id: 'toID',
+            value: toID
+        }).appendTo('.textarea-container');
+    }
 
+    if($('#commentID').length > 0){
+        $('#commentID').attr('value', commentID);
+    }
+    else{
+        $('<input>').attr({
+            type: 'hidden',
+            id: 'commentID',
+            value: commentID
+        }).appendTo('.textarea-container');
+    }
 })
 
-$('#commentBtn').on('click', function () {
+$('#commentBtn').click(function () {
     $.ajax({
         type: 'post',
         url: '/comments/reply',
         data: {
             resourceID:$('#resourceID').val(),
-            content:$('#mainReply').val()
+            content:$('#mainReply').val(),
+            toID: $('#toID').length>0 ? $('#toID').val() : null,
+            commentID: $('#commentID').length>0 ? $('#commentID').val() : null
         },
         dataType: 'json',
         success: function (comments) {
 
             $('#mainReply').val('');
-            console.log('html--评论返回');
-            console.log(comments);
+            // console.log('html--评论返回');
+            // console.log(comments);
             renderComment(comments);
         }
     });
@@ -47,64 +60,31 @@ $.ajax({
         resourceID: $('#resourceID').val()
     },
     success: function(comments) {
-        console.log('html--页面重载');
-        //console.log(comments);
+        // console.log('html--页面重载');
+        // console.log(comments);
         renderComment(comments);
     }
 });
 
-
+// 显示评论列表啦
 function renderComment(comments) {
 
     var len = comments.length;
-    var htmlList = '';
+
     if ( len > 0 ){
         // 总评论条数
         $('.result').html(comments.length);
 
-        // 生成评论列表
+        var htmlList = '',
+            // 模板HTML
+            htmlTemp = $("#template-list").html();
+
         comments.forEach(function (item) {
-            // console.log('遍历');
-            // console.log(item);
-
-            // var time = item.time | date('Y-m-d h:m:s', -8*60);
-            // console.log(time);
-
-            // 二级回复
-            /*var replyList = "";
-            if (item.reply.length > 0){
-
-                item.reply.forEach(function (r) {
-
-                    replyList += '<div class="list-item reply-wrap">' +
-                        '<div class="user-face"><img class="user-head" src="/public/img/res/cat02.png" alt="user"></div>' +
-                        '<div class="reply-con">' +
-                        '<div class="user">' +
-                            '<span class="name">'+ r.from.username +'</span>' +
-                            '<span class="text-con">回复'+ r.to.username +':'+ r.content +'</span></div>' +
-                        '<div class="info">' +
-                            '<span class="time">'+ r.time +'</span>' +
-                            '<span class="like"><i class="glyphicon glyphicon-thumbs-up"></i><span>&nbsp;('+ r.likeCount +')</span>' +
-                            '<span class="reply btn-hover"><a href="#comment-send" data-cid='+ item._id +' data-tid='+ r.form_id +'>回复</a></span>' +
-                        '</div></div></div>';
-                });
-            }*/
-
-            htmlList += '<div class="list-item reply-wrap">' +
-                '<div class="user-face">' +
-                '<img class="user-head" src="/public/img/res/cat01.png" alt="user"></div>' +
-                '<div class="con">' +
-                '<div class="user"><span class="name">' + item.from.username + '</span></div>' +
-                '<p class="text">' + item.content + '</p>' +
-                '<div class="info">' +
-                '<span class="floor">#' + item.floor + '</span>' +
-                '<span class="time">' + item.time + '</span>' +
-                //'<span class="like"><i class="glyphicon glyphicon-thumbs-up"></i><span>&nbsp;(' + item.likeCount + ')</span></span>' +
-                //'<span class="reply btn-hover"><a class="replyyyyy" href="#comment-send" data-cid='+ item._id +' data-tid='+ item.from._id +'>回复</a></span>' +
-                //'</div><div class="replybox">'+ replyList +'</div>' +
-                '</div></div>';
-        });
+            laytpl(htmlTemp).render(item, function(html){
+                htmlList += html;
+            });
+        })
         // 添加到页面
-        $(".comment-list").html(htmlList);
+        $(".comment-list").empty().append(htmlList);
     }
 }
--- routers/comments.js
+++ routers/comments.js
@@ -17,7 +17,7 @@ router.get('/list', function (req, res) {
 
     if(id){
         // 评论数据返回
-        Comments.find({resourceID: id}).populate('from').sort({time: -1}).then(function (comments) {
+        Comments.find({resourceID: id}).populate(['from', 'reply.from', 'reply.to']).sort({time: -1}).then(function (comments) {
             // console.log("ajax 刷新页面返回");
             // console.log(comments);
             res.json(comments);
@@ -34,80 +34,115 @@ router.post('/reply', function (req, res) {
     var resourceID = req.body.resourceID;
     var from = req.userInfo._id;
     var content = req.body.content;
+    var toID = req.body.toID;
+    var commentID = req.body.commentID;
 
+    // console.log('start');
     // console.log(resourceID);
     // console.log(from);
     // console.log(content);
+    // console.log(toID);
+    // console.log(commentID);
 
-    // comments 中是一条评论   同一resourceID对应多条评论  楼层数递增应该在外部统计...麻烦
-    // Resource 中记录当前总评论数floor
-    Resource.findOne({_id: resourceID}).then(function (re) {
 
-        if(re){
-            return Resource.update({_id: resourceID}, {$inc: {floor: 1}}).then(function (re) {
+    if(commentID){
 
-                if (re){
-                    return Resource.findOne({_id: resourceID}, 'floor').then(function (re) {
+        var reply = [];
+        // 找到commentID 把新的回复save
+        Comments.findOne({_id: commentID}).then(function (comment) {
 
-                        if(re){
-                            // 保存当前评论
-                            return new Comments({
-                                resourceID: resourceID,
-                                from: from,
-                                content: content,
-                                floor: re.floor,
-                                time: Date.now()
-                            }).save();
-                        }
-                        else{
-                            console.log('resource 查询未果');
-                            return Promise.reject();
-                        }
-                    })
-                }
-                else {
-                    console.log('update failed');
-                    return Promise.reject();
-                }
+            comment.reply.unshift({
+                from: from,
+                to: toID,
+                content: content,
+                time: Date.now()
+            });
+
+            return comment.save();
+
+        }).then(function (re) {
+
+            console.log('re');
+            console.log(re);
+            // 查询当前resourceID下的所有评论
+            Comments.find({resourceID: resourceID}).populate(['from', 'reply.from', 'reply.to']).sort({time: -1}).then(function (comments) {
+                console.log('评论之后返回数据');
+                console.log(comments.reply);
+                res.json(comments);
             })
-        }
-        else {
-            return Series.update({_id: resourceID}, {$inc: {floor: 1}}).then(function (re) {
+        });
+    }
+    else{
+        // comments 中是一条评论   同一resourceID对应多条评论  楼层数递增应该在外部统计...麻烦
+        // Resource 中记录当前总评论数floor
+        Resource.findOne({_id: resourceID}).then(function (re) {
 
-                if(re){
-                    return Series.findOne({_id: resourceID}, 'floor').then(function (re){
+            if(re){
+                return Resource.update({_id: resourceID}, {$inc: {floor: 1}}).then(function (re) {
 
-                        if (re){
+                    if (re){
+                        return Resource.findOne({_id: resourceID}, 'floor').then(function (re) {
 
-                            // 保存当前评论
-                            return new Comments({
-                                resourceID: resourceID,
-                                from: from,
-                                content: content,
-                                floor: re.floor,
-                                time: Date.now()
-                            }).save();
-                        }
-                        else{
-                            console.log('series 数据未查到')
-                            return Promise.reject();
-                        }
-                    });
-                }
-                else{
-                    console.log('update failed');
-                    return Promise.reject();
-                }
+                            if(re){
+                                // 保存当前评论
+                                return new Comments({
+                                    resourceID: resourceID,
+                                    from: from,
+                                    content: content,
+                                    floor: re.floor,
+                                    time: Date.now()
+                                }).save();
+                            }
+                            else{
+                                console.log('resource 查询未果');
+                                return Promise.reject();
+                            }
+                        })
+                    }
+                    else {
+                        console.log('update failed');
+                        return Promise.reject();
+                    }
+                })
+            }
+            else {
+                return Series.update({_id: resourceID}, {$inc: {floor: 1}}).then(function (re) {
+
+                    if(re){
+                        return Series.findOne({_id: resourceID}, 'floor').then(function (re){
+
+                            if (re){
+
+                                // 保存当前评论
+                                return new Comments({
+                                    resourceID: resourceID,
+                                    from: from,
+                                    content: content,
+                                    floor: re.floor,
+                                    time: Date.now()
+                                }).save();
+                            }
+                            else{
+                                console.log('series 数据未查到')
+                                return Promise.reject();
+                            }
+                        });
+                    }
+                    else{
+                        console.log('update failed');
+                        return Promise.reject();
+                    }
+                })
+            }
+        }).then(function () {
+
+            // 查询当前resourceID下的所有评论
+            Comments.find({resourceID: resourceID}).populate(['from', 'reply.from', 'reply.to']).sort({time: -1}).then(function (comments) {
+                // console.log('评论之后返回数据');
+                res.json(comments);
             })
-        }
-    }).then(function () {
-
-        // 查询当前resourceID下的所有评论
-        Comments.find({resourceID: resourceID}).populate('from').sort({time: -1}).then(function (comments) {
-            // console.log('评论之后返回数据');
-            res.json(comments);
         })
-    })
+    }
 })
 
 module.exports = router;
